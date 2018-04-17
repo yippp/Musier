24e4a293a6f8ef00b43490b11c7bf70ef601cc5a
凡 莫 <115010204@link.cuhk.edu.cn>
2018-04-17 23:36:38 +0800

    Data mining functions.

--- Database/Midi.py
+++ Database/Midi.py
@@ -2,11 +2,11 @@ import mido
 
 
 class Midi:
-
     def __init__(self, file):
         midi = mido.MidiFile(file)
+        self.file_name = file
         self.ticks_per_beat= midi.ticks_per_beat
-        self.notes = []
+        self.messages = []
         self.numerator = None
         self.denominator = None
         self.key = None
@@ -19,9 +19,22 @@ class Midi:
                 self.key = msg.key
             if msg.type == 'set_tempo':
                 self.tempo = msg.tempo
-        beat = 0
+        tick = 0
         for msg in midi.tracks[1]:
-            beat += msg.time / self.ticks_per_beat
-            print(msg.type)
+            tick += msg.time
+            beat = tick / self.ticks_per_beat
             if (msg.type == 'note_on') or (msg.type == 'note_off'):
-                self.notes.append([beat, msg.note, msg.velocity, msg.type])
+                self.messages.append((beat, msg.note, msg.velocity, msg.type))
+
+    def output_as_note_list(self):
+        note_list = []
+        notes_on = {}
+        for msg in self.messages:
+            start = notes_on.get(msg[1])
+            if start is None:
+                notes_on[msg[1]] = msg[0]
+            else:
+                note_list.append((msg[1], start, msg[0] - start))
+                notes_on.pop(msg[1])
+        note_list.sort(key=lambda note: (note[1], note[2]))
+        return note_list
--- Database/analysis.py
+++ Database/analysis.py
@@ -1,5 +1,66 @@
 from Midi import Midi
+import os
+import matplotlib.pyplot as plt
 
 
-midi = Midi('Children/004.mid')
-print(midi.notes)
+def delete_repetition(note_list):
+    new_list = [note_list[0]]
+    for i in range(1, len(note_list)):
+        if note_list[i - 1][0] != note_list[i][0]:
+            new_list.append(note_list[i])
+    return new_list
+
+
+def get_pattern(file_out):
+    direction = 'Pop'
+    files = os.listdir(direction)
+    pattern_dict = {}
+    for file in files:
+        path = direction + '/' + file
+        if os.path.isfile(path):
+            midi = Midi(path)
+            note_list = midi.output_as_note_list()
+            # note_list = delete_repetition(note_list)
+            for i in range(1, len(note_list)):
+                pattern = note_list[i][0] - note_list[i - 1][0]
+                # pattern = note_list[i][0] + note_list[i - 2][0] - note_list[i - 1][0] * 2  # Define your pattern here
+                if pattern_dict.get(pattern) is not None:
+                    pattern_dict[pattern] += 1
+                else:
+                    pattern_dict[pattern] = 1
+    pattern_list = list(pattern_dict.items())
+    pattern_list.sort(key=lambda item: -item[1])
+    f = open(file_out, 'w')
+    for i in range(len(pattern_list)):
+        f.write("%d %d\n" % (pattern_list[i][0], pattern_list[i][1]))
+    f.close()
+    return pattern_list
+
+
+def plot(xlabel, ylabel, file_in, file_out, num = None):
+    f = open(file_in, 'r')
+    x = []
+    y = []
+    label = []
+    i = 0
+    for line in f.readlines():
+        x.append(i)
+        v1, v2 = tuple(map(int, line.split()))
+        y.append(v2)
+        label.append(v1)
+        i += 1
+    f.close()
+    x = x[:num]
+    y = y[:num]
+    label = label[:num]
+    plt.figure(figsize=(10, 4))
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.xticks(x, label)
+    plt.bar(x, y, 0.5, align='center')
+    plt.savefig(file_out)
+    plt.show()
+
+
+get_pattern('diff-freq.txt')
+plot('Difference', 'Frequency', 'diff-freq.txt', 'diff-freq.jpg', 24)
