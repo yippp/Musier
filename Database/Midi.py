d124319e6490a88ee3e2f04f816a2f72cbdab86e
凡 莫 <115010204@link.cuhk.edu.cn>
2018-04-18 19:05:32 +0800

    Added 'midi2period' function; Generated periods and abcjs strings and wrote them in files

--- Database/Midi.py
+++ Database/Midi.py
@@ -1,4 +1,5 @@
 import mido
+from GAModel.period import unit, period
 
 
 class Midi:
@@ -26,7 +27,7 @@ class Midi:
             if (msg.type == 'note_on') or (msg.type == 'note_off'):
                 self.messages.append((beat, msg.note, msg.velocity, msg.type))
 
-    def output_as_note_list(self):
+    def output_as_note_list(self, normalize=False):
         note_list = []
         notes_on = {}
         for msg in self.messages:
@@ -34,7 +35,39 @@ class Midi:
             if start is None:
                 notes_on[msg[1]] = msg[0]
             else:
-                note_list.append((msg[1], start, msg[0] - start))
+                end = msg[0]
+                if normalize:
+                    start = round(start * 4) / 4
+                    end = round(end * 4) / 4
+                note_list.append((msg[1], start, end - start))
                 notes_on.pop(msg[1])
         note_list.sort(key=lambda note: (note[1], note[2]))
         return note_list
+
+    def output_as_period(self, scale=16, base=72):
+        calibration = self.denominator / scale
+        note_list = self.output_as_note_list(normalize=True)
+        index = 1
+        while index < len(note_list):  # Clean overlapped notes
+            if note_list[index][1] < note_list[index - 1][1] + calibration:
+                del note_list[index]
+            else:
+                index += 1
+        bias = (note_list[0][1] // self.numerator) * self.numerator
+        index = 0
+        p = period()
+        while True:
+            last = bias
+            u = []
+            while (index < len(note_list)) and (note_list[index][1] < bias + self.numerator):
+                u.extend([None] * int((note_list[index][1] - last) // calibration))
+                u.append(note_list[index][0] - base)
+                last = note_list[index][1] + calibration
+                index += 1
+            u.extend([None] * int((bias + self.numerator - last) // calibration))
+            if len(u) != self.numerator // calibration:
+                return bias
+            p.append(unit(u))
+            if index == len(note_list):
+                return p
+            bias += self.numerator
--- Database/analysis.py
+++ Database/analysis.py
@@ -21,23 +21,27 @@ def get_pattern(file_out):
             midi = Midi(path)
             note_list = midi.output_as_note_list()
             # note_list = delete_repetition(note_list)
-            for i in range(1, len(note_list)):
+            for i in range(2, len(note_list)):
                 pattern = note_list[i][0] - note_list[i - 1][0]
                 # pattern = note_list[i][0] + note_list[i - 2][0] - note_list[i - 1][0] * 2  # Define your pattern here
+                # pattern = (note_list[i - 1][0] - note_list[i - 2][0], note_list[i][0] - note_list[i - 1][0])
                 if pattern_dict.get(pattern) is not None:
                     pattern_dict[pattern] += 1
                 else:
                     pattern_dict[pattern] = 1
     pattern_list = list(pattern_dict.items())
     pattern_list.sort(key=lambda item: -item[1])
+    file_out = 'results/' + file_out
     f = open(file_out, 'w')
     for i in range(len(pattern_list)):
+        # f.write("%d %d %d\n" % (pattern_list[i][0][0], pattern_list[i][0][1], pattern_list[i][1]))
         f.write("%d %d\n" % (pattern_list[i][0], pattern_list[i][1]))
     f.close()
     return pattern_list
 
 
 def plot(xlabel, ylabel, file_in, file_out, num = None):
+    file_in = 'results/' + file_in
     f = open(file_in, 'r')
     x = []
     y = []
@@ -46,21 +50,33 @@ def plot(xlabel, ylabel, file_in, file_out, num = None):
     for line in f.readlines():
         x.append(i)
         v1, v2 = tuple(map(int, line.split()))
+        # v1, v2, v3 = tuple(map(int, line.split()))
         y.append(v2)
         label.append(v1)
+        # y.append(v3)
+        # label.append((v1, v2))
         i += 1
     f.close()
     x = x[:num]
     y = y[:num]
     label = label[:num]
-    plt.figure(figsize=(10, 4))
+    plt.figure(figsize=(20, 4))
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.xticks(x, label)
     plt.bar(x, y, 0.5, align='center')
+    file_out = 'results/' + file_out
     plt.savefig(file_out)
     plt.show()
 
 
+# get_pattern('difftuple-freq.txt')
+# plot('Difference Tuple', 'Frequency', 'difftuple-freq.txt', 'difftuple-freq.jpg', 32)
+# get_pattern('2nddiff-freq.txt')
+# plot('Second Difference', 'Frequency', '2nddiff-freq.txt', '2nddiff-freq.jpg', 32)
+# get_pattern('2nddiff-freq(without repetition).txt')
+# plot('Second Difference', 'Frequency', '2nddiff-freq(without repetition).txt', '2nddiff-freq(without repetition).jpg', 32)
+# get_pattern('diff-freq(without repetition).txt')
+# plot('Difference', 'Frequency', 'diff-freq(without repetition).txt', 'diff-freq(without repetition).jpg', 32)
 get_pattern('diff-freq.txt')
-plot('Difference', 'Frequency', 'diff-freq.txt', 'diff-freq.jpg', 24)
+plot('Difference', 'Frequency', 'diff-freq.txt', 'diff-freq.jpg', 32)
